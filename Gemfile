5e83865eaf9556e3cb9be1b878d8d82c8c01610a
antonio <antoniogomesdev@gmail.com>
2022-12-15 14:26:51 -0300

    added password encryption and rspec

--- Gemfile
+++ Gemfile
@@ -18,6 +18,8 @@ gem 'jwt'
 
 gem 'byebug'
 
+gem 'rspec'
+
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 # gem "jbuilder"
 
@@ -28,7 +30,7 @@ gem 'byebug'
 # gem "kredis"
 
 # Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
-# gem "bcrypt", "~> 3.1.7"
+gem "bcrypt", "~> 3.1.7"
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_14_223408) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_15_152823) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_14_223408) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_boards_on_user_id"
   end
 
   create_table "items", force: :cascade do |t|
@@ -39,11 +41,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_14_223408) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
-    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
+  add_foreign_key "boards", "users"
   add_foreign_key "items", "sections"
   add_foreign_key "sections", "boards"
 end
