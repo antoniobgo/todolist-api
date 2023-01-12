db6c5fa868ec4772ef633a2ce673baf5184db34b
antonio <antoniogomesdev@gmail.com>
2023-01-11 23:36:14 -0300

    minor error handling improvements

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -2,11 +2,11 @@
 
 class BoardsController < ApplicationController
   before_action :authorized
+  rescue_from ActiveRecord::RecordNotFound, with: :handle_record_not_found
 
   # GET /todos
   def index
     boards = Board.all
-
     render json: boards, status: 200
   end
 
@@ -21,13 +21,29 @@ class BoardsController < ApplicationController
     end
   end
 
+  def update
+    board = Board.find(params[:id])
+    board.title = params[:title]
+
+    if board.save
+      render json: {board: board.as_json(only: [:id, :title])}, status: 200
+    else
+      render json: {message: "couldnt create board"}, status: :unprocessable_entity
+    end
+  end
+
   def user_boards
-    render json: current_user.boards
+    # boards = Board.find_by(user_id: current_user.id)
+    render json: current_user.boards.sort_by(&:id)
   end
 
   private
 
   def board_params
     params.permit(:title)
+  end
+
+  def handle_record_not_found(e)
+    render json: { message: 'Board not found' }, status: :unprocessable_entity
   end
 end
